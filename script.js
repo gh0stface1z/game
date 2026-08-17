276651cb4e72d3257aac930b1b79ccb460fdd65c
gh0stface1z <julliajutotu120121@gmail.com>
2026-08-17 16:23:34 -0400

    Remove desk interaction functionality

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const room = document.getElementById("room");
-const desk = document.getElementById("desk");
-const backButton = document.getElementById("back-button");
 
-// CLICAR NA MESA
-desk.addEventListener("click", () => {
-    room.classList.add("zoom-desk");
-    backButton.style.display = "block";
-});
-
-// VOLTAR PARA O QUARTO INTEIRO
-backButton.addEventListener("click", () => {
-    room.classList.remove("zoom-desk");
-    backButton.style.display = "none";
-});
